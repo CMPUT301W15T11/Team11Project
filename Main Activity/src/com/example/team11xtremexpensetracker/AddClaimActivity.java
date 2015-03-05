bb19ba19e24381b21ec96f51ff944ca3bed80ac0
joshdeng <j.deng7@Hotmail.com>
2015-03-05 14:51:23 -0700

    bug fixed

--- Main Activity/src/com/example/team11xtremexpensetracker/AddClaimActivity.java
+++ Main Activity/src/com/example/team11xtremexpensetracker/AddClaimActivity.java
@@ -1,15 +1,69 @@
 package com.example.team11xtremexpensetracker;
 
+import java.util.Calendar;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 
 public class AddClaimActivity extends Activity {
-
+	
+	private String claimName;
+	private Calendar startDate;
+	private Calendar endDate;
+	private Button startDatePickerButton;
+	private Button endDatePickerButton;
+	private Button doneButton;
+	private Intent intent;
+	
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_claim);
+		// get buttons
+		startDatePickerButton = (Button) findViewById(R.id.startDatePickerButton);
+		endDatePickerButton = (Button) findViewById(R.id.endDatePickerButton);
+		doneButton = (Button) findViewById(R.id.addClaimDoneButton);
+		
+		startDatePickerButton.setOnClickListener(new View.OnClickListener() {	
+			Calendar c = Calendar.getInstance();
+			@Override
+			public void onClick(View v) {
+				new SingleDatePickerDialog(AddClaimActivity.this, 0, new SingleDatePickerDialog.OnDateSetListener() {
+
+					@Override
+					public void onDateSet(DatePicker startDatePicker, int startYear, int startMonthOfYear,
+							int startDayOfMonth) {
+						String textString_start = String.format("%d-%d-%d", startYear, startMonthOfYear + 1,
+								startDayOfMonth);
+						
+					}
+				}, c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DATE), true).show();
+			}
+			
+		});
+		
+		endDatePickerButton.setOnClickListener(new View.OnClickListener() {	
+			@Override
+			public void onClick(View v) {
+				
+			}
+		});
+		
+		
+		endDatePickerButton.setOnClickListener(new View.OnClickListener() {	
+			@Override
+			public void onClick(View v) {
+				
+			}
+		});
+		
 	}
 
 	@Override
