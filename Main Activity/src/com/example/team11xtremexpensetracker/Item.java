eac49597b954611e289ac429174e562d3dc87531
Stinn <justin_lu55@hotmail.com>
2015-03-17 13:05:52 -0600

    javadoc

--- Main Activity/src/com/example/team11xtremexpensetracker/Item.java
+++ Main Activity/src/com/example/team11xtremexpensetracker/Item.java
@@ -1,6 +1,12 @@
 package com.example.team11xtremexpensetracker;
 
 import java.util.Calendar;
+/**
+ * Item structure
+ * use to store items
+ * @author Stin
+ *
+ */
 
 public class Item {
 	private String item;
@@ -10,45 +16,101 @@ public class Item {
 	private String unit;
 	private String description;
 	private Boolean indicator = false;
+	/**
+	 * get item name
+	 * @return
+	 */
 	public String getItem() {
 		return item;
 	}
+	/**
+	 * set item name
+	 * @param item
+	 */
 	public void setItem(String item) {
 		this.item = item;
 	}
+	/**
+	 * get item date
+	 * @return
+	 */
 	public Calendar getDate() {
 		return date;
 	}
+	/**
+	 * set item date
+	 * @param date
+	 */
 	public void setDate(Calendar date) {
 		this.date = date;
 	}
+	/**
+	 * get item category
+	 * @return
+	 */
 	public String getCategory() {
 		return category;
 	}
+	/**
+	 * set item category
+	 * @param category
+	 */
 	public void setCategory(String category) {
 		this.category = category;
 	}
+	/**
+	 * get item amount spent
+	 * @return
+	 */
 	public String getAmount() {
 		return amount;
 	}
+	/**
+	 * set item amount spent
+	 * @param amount
+	 */
 	public void setAmount(String amount) {
 		this.amount = amount;
 	}
+	/**
+	 * get item unit of currency
+	 * @return
+	 */
 	public String getUnit() {
 		return unit;
 	}
+	/**
+	 * set item unit of currency
+	 * @param unit
+	 */
 	public void setUnit(String unit) {
 		this.unit = unit;
 	}
+	/**
+	 * get item description
+	 * @return
+	 */
 	public String getDescription() {
 		return description;
 	}
+	/**
+	 * set item description
+	 * @param description
+	 */
 	public void setDescription(String description) {
 		this.description = description;
 	}
+	/**
+	 * get item indicator
+	 * @return
+	 */
 	public Boolean getIndecator(){
 		return indicator;
 	}
+	/**
+	 * item set indicator
+	 * @param indicator
+	 */
 	public void setIndecator(Boolean indicator){
 		this.indicator = indicator;
 	}
