a399835a663bcd1181ebcdcce2d0ec7457b8b5e9
FadedWill <mingtuo@ualberta.ca>
2015-03-19 14:57:24 -0600

    tag filter bug fixed

--- Main Activity/src/com/example/team11xtremexpensetracker/activity/ListClaimsActivity.java
+++ Main Activity/src/com/example/team11xtremexpensetracker/activity/ListClaimsActivity.java
@@ -38,140 +38,162 @@ import android.widget.Toast;
 
 public class ListClaimsActivity extends Activity {
 	private ClaimsList datafile;
-	//private ListClaimsAdapter listClaimAdapter;
+	// private ListClaimsAdapter listClaimAdapter;
 	private ListView claimsListView;
 	private ImageView searchImage;
 	private ClaimListController clc;
 	private EditText tagSearchEdit;
 	private int onLongClickPos;
-	private Collection<ExpenseClaim> claims; 
+	private Collection<ExpenseClaim> claims;
 	private ClaimsList dataList;
 	private static final String FILENAME = "datafile.sav";
-	
+
+	private ArrayList<Integer> indexCorrector;
+	private boolean filterFlag;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_list_claims);
-		claimsListView = (ListView) findViewById(R.id.claimsListView); 
-		searchImage=(ImageView)findViewById(R.id.tag_search);
-		tagSearchEdit=(EditText)findViewById(R.id.tag_filter);
-		
-		dataList=this.loadFromFile();
-		clc = new ClaimListController(); 
+		claimsListView = (ListView) findViewById(R.id.claimsListView);
+		searchImage = (ImageView) findViewById(R.id.tag_search);
+		tagSearchEdit = (EditText) findViewById(R.id.tag_filter);
+
+		dataList = this.loadFromFile();
+		clc = new ClaimListController();
 		clc.setClaimsList(dataList);
-		claims = clc.getClaimsList().getClaims(); 
+		claims = clc.getClaimsList().getClaims();
 		final ArrayList<ExpenseClaim> list = new ArrayList<ExpenseClaim>(claims);
-		final ArrayAdapter<ExpenseClaim> claimAdapter = new ArrayAdapter<ExpenseClaim>(this, android.R.layout.simple_expandable_list_item_1, list);
+		final ArrayAdapter<ExpenseClaim> claimAdapter = new ArrayAdapter<ExpenseClaim>(this,
+				android.R.layout.simple_expandable_list_item_1, list);
 		claimsListView.setAdapter(claimAdapter);
-		
-		clc.getClaimsList().addListener(new Listener () {
+
+		indexCorrector = new ArrayList<Integer>();
+		filterFlag = false;
+
+		clc.getClaimsList().addListener(new Listener() {
 			@Override
-			public void update(){
+			public void update() {
 				list.clear();
 				Collection<ExpenseClaim> claims = (clc.getClaimsList().getClaims());
-				if(claims==null){
+				if (claims == null) {
 					return;
 				}
 				list.addAll(claims);
 				claimAdapter.notifyDataSetChanged();
 			}
 		});
-		
+
 		/**
-		 * set Listener to search button, which controls the function of claim filter
+		 * set Listener to search button, which controls the function of claim
+		 * filter
 		 */
-		searchImage.setOnClickListener(new View.OnClickListener(){
+		searchImage.setOnClickListener(new View.OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				String searchingTag=tagSearchEdit.getText().toString();
+				String searchingTag = tagSearchEdit.getText().toString();
 				searchingTag.toLowerCase();
-				if(searchingTag.equals("")){
+				if (searchingTag.equals("")) {
+					filterFlag = false;
 					list.clear();
 					Collection<ExpenseClaim> claims = (clc.getClaimsList().getClaims());
 					list.addAll(claims);
 					claimAdapter.notifyDataSetChanged();
 					return;
 				}
+				filterFlag = true;
+				indexCorrector.clear();
 				ArrayList<ExpenseClaim> claims = (clc.getClaimsList().getClaimsAL());
 				list.clear();
-				for(int i=0;i<claims.size();i++){
-					for(int j=0;j<claims.get(i).getTagList().size();j++){
-						if(searchingTag.equals(claims.get(i).getTagList().get(j).getTagName().toLowerCase())){
+				for (int i = 0; i < claims.size(); i++) {
+					for (int j = 0; j < claims.get(i).getTagList().size(); j++) {
+						if (searchingTag.equals(claims.get(i).getTagList().get(j).getTagName().toLowerCase())) {
 							list.add(claims.get(i));
+							indexCorrector.add(i);
 						}
 					}
 				}
 				claimAdapter.notifyDataSetChanged();
 			}
-			
+
 		});
-		
-		
+
 		claimsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 			@Override
 			public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
-				String temp = list.get(position).toString();
-				//Toast.makeText(ListClaimsActivity.this, "You Clicked " + temp, Toast.LENGTH_SHORT).show();
-				//Now make the same intent push as in the addClaimActivity.
-				saveInFile();
-				Intent intent = new Intent(ListClaimsActivity.this, ViewClaimActivity.class);
-				intent.putExtra("claimID", position);
-				startActivity(intent);
+				// String temp = list.get(position).toString();
+				// Toast.makeText(ListClaimsActivity.this, "You Clicked " +
+				// temp, Toast.LENGTH_SHORT).show();
+				// Now make the same intent push as in the addClaimActivity.
+				// saveInFile();
+				if (!filterFlag) {
+					Intent intent = new Intent(ListClaimsActivity.this, ViewClaimActivity.class);
+					intent.putExtra("claimID", position);
+					startActivity(intent);
+				} else {
+					int correctIndex = indexCorrector.get(position);
+					Intent intent = new Intent(ListClaimsActivity.this, ViewClaimActivity.class);
+					intent.putExtra("claimID", correctIndex);
+					startActivity(intent);
+				}
+
 			}
 		});
-		
-		
-		
-		
+
 		claimsListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
 
-			
-
 			@Override
-			public boolean onItemLongClick(AdapterView<?> parent, View view,
-					int position, long id) {
+			public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
 				// TODO Auto-generated method stub
 				onLongClickPos = position;
 				AlertDialog.Builder adb = new AlertDialog.Builder(ListClaimsActivity.this);
 				adb.setMessage("Delete?");
 				adb.setCancelable(true);
-				adb.setPositiveButton("Delete", new OnClickListener(){
+				adb.setPositiveButton("Delete", new OnClickListener() {
 
 					@Override
 					public void onClick(DialogInterface dialog, int which) {
-						
-						ClaimListController.removeClaim(onLongClickPos);
-						
-						claimAdapter.clear();
-						for (int i=0;i<ClaimListController.getClaimsList().getLength();i++){
-							claimAdapter.add(ClaimListController.getClaimsList().getClaimById(i));
+						if (!filterFlag) {
+							ClaimListController.removeClaim(onLongClickPos);
+							claimAdapter.clear();
+							for (int i = 0; i < ClaimListController.getClaimsList().getLength(); i++) {
+								claimAdapter.add(ClaimListController.getClaimsList().getClaimById(i));
+							}
+							saveInFile();
+							dataList = loadFromFile();
+							ClaimListController.setClaimsList(dataList);
+							claimAdapter.notifyDataSetChanged();
+						} else {
+							int correctIndex = indexCorrector.get(onLongClickPos);
+							indexCorrector.remove(onLongClickPos);
+							list.remove(onLongClickPos);
+							ClaimListController.removeClaim(correctIndex);
+							saveInFile();
+							dataList = loadFromFile();
+							ClaimListController.setClaimsList(dataList);
+							claimAdapter.notifyDataSetChanged();
 						}
-						
-						saveInFile();
-						dataList=loadFromFile();
-						ClaimListController.setClaimsList(dataList);
-						claimAdapter.notifyDataSetChanged();
-						}
-									
+
+					}
+
 				});
-				adb.setNegativeButton("Cancel", new OnClickListener(){
+				adb.setNegativeButton("Cancel", new OnClickListener() {
 
 					@Override
 					public void onClick(DialogInterface dialog, int which) {
 						// TODO Auto-generated method stub
-						
+
 					}
-					
+
 				});
 				adb.show();
 				return true;
 			}
 		});
-	
+
 	}
-	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -179,22 +201,22 @@ public class ListClaimsActivity extends Activity {
 		getMenuInflater().inflate(R.menu.list_claims, menu);
 		return true;
 	}
-	
-	public void addClaim(View v){
-		//Toast.makeText(this, "Adding Claim", Toast.LENGTH_LONG).show();
+
+	public void addClaim(View v) {
+		// Toast.makeText(this, "Adding Claim", Toast.LENGTH_LONG).show();
 		Intent intent = new Intent(ListClaimsActivity.this, AddClaimActivity.class);
 		intent.putExtra("claimID", -1);
 		startActivity(intent);
 	}
-	
+
 	@Override
-	public void onBackPressed(){
+	public void onBackPressed() {
 		Intent intentBackPressed = new Intent();
 		intentBackPressed.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 		intentBackPressed.setClass(ListClaimsActivity.this, MainActivity.class);
 		startActivity(intentBackPressed);
 	}
-	
+
 	private ClaimsList loadFromFile() {
 		Gson gson = new Gson();
 		dataList = new ClaimsList();
@@ -231,5 +253,5 @@ public class ListClaimsActivity extends Activity {
 			e.printStackTrace();
 		}
 	}
-	
+
 }
