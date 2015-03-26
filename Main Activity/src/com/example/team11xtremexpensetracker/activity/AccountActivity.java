3ef3c898b19b5e60b009726ba2c2a82061282dba
FadedWill <mingtuo@ualberta.ca>
2015-03-25 19:10:32 -0600

    Login activity removed, now only need to enter name

--- Main Activity/src/com/example/team11xtremexpensetracker/activity/AccountActivity.java
+++ Main Activity/src/com/example/team11xtremexpensetracker/activity/AccountActivity.java
@@ -9,8 +9,6 @@ import com.example.team11xtremexpensetracker.R;
 import com.example.team11xtremexpensetracker.R.id;
 import com.example.team11xtremexpensetracker.R.layout;
 import com.example.team11xtremexpensetracker.R.menu;
-import com.example.team11xtremexpensetracker.RegisterActivity;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -27,21 +25,15 @@ public class AccountActivity extends Activity {
 	private static final String FILENAME = "datafile.sav";
 	private ClaimListController clc;
 
-	private EditText accountIdEdit;
-	private EditText accountPasswordEdit;
-	private String accountId;
-	private String password;
+
 	private Button backButton;
 	private Button confirmButton;
-	private Button registerButton;
 
 	private void init() {
 
-		accountIdEdit = (EditText) findViewById(R.id.account_id_view);
-		accountPasswordEdit = (EditText) findViewById(R.id.account_password_view);
+		
 		backButton = (Button) findViewById(R.id.account_back);
 		confirmButton = (Button) findViewById(R.id.account_confirm);
-		registerButton=(Button) findViewById(R.id.register_new_button);
 
 		backButton.setOnClickListener(new View.OnClickListener() {
 
@@ -53,49 +45,18 @@ public class AccountActivity extends Activity {
 		});
 
 		confirmButton.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				accountId = accountIdEdit.getText().toString();
-				password = accountPasswordEdit.getText().toString();
-				//if (checkId(accountId) && checkPassword(password)) {
-					// TODO check userID exists or not,if so go next step
-					// TODO check every claim's userID and add them into next activity
-					
-					//
-					Intent intent=new Intent(AccountActivity.this,ListClaimsActivity.class);
-					startActivity(intent);
-				//} else {
-				//	Toast.makeText(AccountActivity.this, "Invalid ID or password!", Toast.LENGTH_SHORT).show();
-				//	accountPasswordEdit.setText("");
-				//}
-			}
-		});
-		
-		registerButton.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Intent intent =new Intent(AccountActivity.this,RegisterActivity.class);
+				Intent intent=new Intent(AccountActivity.this,ListClaimsActivity.class);
+				intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 				startActivity(intent);
+				
 			}
 		});
-	}
-
-	private boolean checkId(String accountId) {
-		String strPattern = "^[a-zA-Z][\\w\\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\\w\\.-]*[a-zA-Z0-9]\\.[a-zA-Z][a-zA-Z\\.]*[a-zA-Z]$";
-		Pattern p = Pattern.compile(strPattern);
-		Matcher m = p.matcher(accountId);
-		return m.matches();
-	}
-
-	private boolean checkPassword(String accountPassword) {
-		if (accountPassword.length() < 8) {
-			return false;
-		}
-		return true;
+		
+		
 	}
 
 	@Override
