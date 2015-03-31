517aef7a470e91a7e3fa40df6a45de3fc7fb74b6
chasemc67 <chasemc67@gmail.com>
2015-03-31 14:23:44 -0600

    Added test cases for adding claims

--- TestActivity/src/com/example/team11xtremexpensetracker/activity/ListClaimsActivityTest.java
+++ TestActivity/src/com/example/team11xtremexpensetracker/activity/ListClaimsActivityTest.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class ListClaimsActivityTest extends ActivityInstrumentationTestCase2<ListClaimsActivity> {
 
@@ -22,6 +23,8 @@ public class ListClaimsActivityTest extends ActivityInstrumentationTestCase2<Lis
 	Button addClaimButton;
 	ListView claimsListView;
 	ListClaimsActivity mActivity;
+	//Values To be used to create claim
+	String claimName = "Maui";
 	//ListClaimsActivity mActivity2;
 	int origNumberClaims;
 	int newNumClaims;
@@ -121,7 +124,7 @@ public class ListClaimsActivityTest extends ActivityInstrumentationTestCase2<Lis
 	public void testAddClaimInAddClaimActivity() throws Exception {
 		ActivityMonitor activityMonitor = getInstrumentation().addMonitor(AddClaimActivity.class.getName(), null, false);
 		ActivityMonitor activityMonitor2 = getInstrumentation().addMonitor(ViewClaimActivity.class.getName(), null, false);
-		final EditText claimName;
+		final EditText claimNameTextView;
 		final Button doneButton;
 		
 		
@@ -136,12 +139,12 @@ public class ListClaimsActivityTest extends ActivityInstrumentationTestCase2<Lis
 		});
 		
 		AddClaimActivity addClaimActivity = (AddClaimActivity)getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 0);
-		claimName = (EditText)addClaimActivity.findViewById(R.id.editTextEnterName);
+		claimNameTextView = (EditText)addClaimActivity.findViewById(R.id.editTextEnterName);
 		doneButton = (Button)addClaimActivity.findViewById(R.id.addClaimDoneButton);
 		addClaimActivity.runOnUiThread(new Runnable() {
 			@Override
 			public void run(){
-				claimName.setText("Maui");
+				claimNameTextView.setText(claimName);
 				doneButton.performClick();
 			}
 		});
@@ -152,31 +155,78 @@ public class ListClaimsActivityTest extends ActivityInstrumentationTestCase2<Lis
 		vClaim.finish();
 		assertNotNull(mActivity);
 		tearDown();
-	}
-	
-	public void testClaimNumberWentUp() throws Exception{
+		
+		/* 
+		 * Re setup to get the new number of claims
+		 */
+		setUp();
 		newNumClaims = claimsListView.getAdapter().getCount();
 		assertTrue("Old: " + new Integer(origNumberClaims).toString() + " New: " + new Integer(newNumClaims).toString(), newNumClaims > origNumberClaims);
+		tearDown();
+		
+	}
+	
+	/*
+	 * Test clicking the last item in listView and check if the proper viewClaim opens
+	 */
+	public void testClickToClaimViewShowsCorrectCaim() throws Exception {
+		ActivityMonitor activityMonitor = getInstrumentation().addMonitor(ViewClaimActivity.class.getName(), null, false);
+		final int listCount = (claimsListView.getAdapter().getCount()) -1;
+		
+		//ViewClaimAcitivty UI Elemtents
+		final TextView claimNameTextView;
+		
+		mActivity.runOnUiThread(new Runnable() {
+			@Override 
+			public void run(){
+				claimsListView.performItemClick(claimsListView, listCount, 0);	
+			}
+		});
+		ViewClaimActivity vActivity = (ViewClaimActivity)getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 0);
+		claimNameTextView = (TextView)vActivity.findViewById(R.id.textViewClaimName);
+		assertTrue("Claim Name not what was expected" ,claimNameTextView.getText().equals(claimName));
+		vActivity.finish();
 		tearDown();
 	}
 	
 	/*
-	 * Cant simulate listView itemLongClick with test equipment. so cant check this yet
-	public void testDeleteClaim() throws Exception{
+	 * test adding an expense to a claims
+	 */
+	public void testAddExpenseToClaim() throws Exception {
+		ActivityMonitor activityMonitor = getInstrumentation().addMonitor(ViewClaimActivity.class.getName(), null, false);
+		ActivityMonitor activityMonitor2 = getInstrumentation().addMonitor(AddItemActivity.class.getName(), null, false);
+		ViewClaimActivity vActivity;
+		AddItemActivity iActivity;
+		final int listCount = (claimsListView.getAdapter().getCount()) -1;
+		//ViewClaimAcitivty UI Elemtents
+		final Button addExpenseButton;
+		//AddItemActivity UI Elements
+		
+		
 		mActivity.runOnUiThread(new Runnable() {
+			@Override 
+			public void run(){
+				claimsListView.performItemClick(claimsListView, listCount, 0);	
+			}
+		});
+		vActivity = (ViewClaimActivity)getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 0);
+		addExpenseButton = (Button)vActivity.findViewById(R.id.buttonClaimAddExpense);
+		
+		vActivity.runOnUiThread(new Runnable() {
 			@Override
 			public void run(){
-				claimsListView.requestFocusFromTouch();
-				claimsListView.setSelection(0);
-				claimsListView.performItemClick(claimsListView.getAdapter().getView(0, null, null), 0, claimsListView.getAdapter().getItemId(0));
-				
+				addExpenseButton.performClick();
 			}
 		});
 		
+		iActivity = (AddItemActivity)getInstrumentation().waitForMonitorWithTimeout(activityMonitor2, 0);
 		
-	}*/
-	
-	
+		//Add the expense stuff here.
+		
+		iActivity.finish();
+		vActivity.finish();
+		tearDown();
+	}
 }
 
 
